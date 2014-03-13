389afa37cdb4a6cf5f48a355b73c75853527eda3
Jan-Paul van Burgsteden <jp@true.nl>
2014-03-14 00:33:06 +0100

    Added condition to match when darkness is imminent
    Logic is flawed when subtracting time from sunsettime just after midnight.

--- darkness.lua
+++ darkness.lua
@@ -9,19 +9,25 @@
 
 local margin = 30;  -- amount of minutes before sunset/after sunrise we should change the Darkness value
 
+local currenttime;
+local currenthour;
 local sunsettime;
 local sunset;
 local sunrise;
 local sunrisetime;
 
 while true do
+  currenttime = os.date('*t');
+  currenthour = currenttime['hour'];
   sunsettime  = os.date("%H:%M", os.time()+margin * 60);
   sunrisetime = os.date("%H:%M", os.time()-margin * 60);
   
   sunset  = fibaro:getValue(1, "sunsetHour");
   sunrise = fibaro:getValue(1, "sunriseHour");
 
-  if ( sunsettime >= sunset or sunrisetime <= sunrise ) then
+  if ( currenthour > 22 or currenthour < 7 ) then -- it's always dark between 22 and 7
+    fibaro:setGlobal("Darkness", "1");
+  elseif ( sunsettime >= sunset or sunrisetime <= sunrise ) then
     fibaro:setGlobal("Darkness", "1");
   else
     fibaro:setGlobal("Darkness", "0");
